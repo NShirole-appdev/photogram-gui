124154df030633a051efbb65dd69902baf1c7ef0
Nina Shirole <ninashirole@uchicago.edu>
2021-08-04 05:21:12 +0000

    user details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,17 @@ class UsersController < ApplicationController
     matching_users = User.all
     @list_of_users = matching_users.order({ :username => :asc })
 
-
     render({ :template => "user_templates/index.html.erb"})
+  end
+
+  def show
+    #parameter is path_username = anisa
+    
+    url_username = params.fetch("path_username")
+    matching_usernames = User.where({ :username => url_username})
+
+    @the_user = matching_usernames.first
+    
+    render({ :template => "user_templates/show.html.erb"})
   end
 end
